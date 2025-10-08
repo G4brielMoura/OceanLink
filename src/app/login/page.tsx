48c71c10c94381a8736516380981cac5e6eff269
G4brielMoura <pereirbiel@gmail.com>
2025-10-08 18:53:31 -0300

    gap_login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -65,7 +65,7 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-sky-100 to-blue-200">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-sky-100 to-blue-200 px-4 sm:px-6 md:px-0">
       <motion.div
         initial={{ opacity: 0, y: 40 }}
         animate={{ opacity: 1, y: 0 }}
